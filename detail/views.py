f13316bcd76a2446cc7da2108d6e778b1f572e32
Kunal7069 <jaink7069@gmail.com>
2024-05-12 21:06:12 +0530

    DONE

--- detail/views.py
+++ detail/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from django.http import HttpResponse
 from rest_framework import status
 from django.shortcuts import render
-from model.models import *
 import requests
 from rest_framework.generics import ListAPIView
 # Create your views here.
